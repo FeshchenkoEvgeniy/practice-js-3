bf0619dafe4600b5f449dbe442e53f550c03021e
Evgeniy <feshchenko.evgeniy03@gmail.com>
2023-01-10 23:02:12 +0200

    update file

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log("fsdfsdf")
-console.log(galleryItems);
+
+const boxGallery = document.querySelector('.gallery');
+
+const createMarkup = galleryItems.map(({ preview, original, description }) => 
+`<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join("");
+
+boxGallery.insertAdjacentHTML("beforeend", createMarkup);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,9 @@
+import Player from "@vimeo/player";
 
+let iframe = document.querySelector("#vimeo-player");
+console.log(iframe)
+    var player = new Player(iframe);
+
+    player.on('play', function() {
+      console.log('Played the video');
+    });
